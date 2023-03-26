8107ddec1392b8d9b18713a8ab8fc92484e42c37
ADRian123L <adrian163@usf.edu>
2023-03-26 12:11:12 -0400

    Created an object for the widgets in main.

--- source/main.py
+++ source/main.py
@@ -1,14 +1,6 @@
-from api import OpenAI
-import openai
-import os
+from widgets import Decimal_Binary_Convert as Bin
 
-import openai
 
-# Set your API key
-key = os.getenv("OPENAI_API_KEY")
-AI: OpenAI = OpenAI(api_key = key)
-AI.set_prompt("Testing the OpenAI API, can you tell something about AI?")
-models = openai.Engine.list()
-data = models["data"]
-idt = data[0].id
-print(idt)
+if __name__ == '__main__':
+    wid: bin = Bin()
+    wid.do()
